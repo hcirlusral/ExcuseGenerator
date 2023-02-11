8151b8e9687516e8f6753b41a7667d2243e6b9f5
hcirlusral <giulianagamba97@gmail.com>
2023-02-11 00:34:52 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["My", ""];
+  let subject = ["grandma", "cat", "giraffe", "teacher"];
+  let action = [
+    "ate my",
+    "threw my",
+    "stole my",
+    "yelled at my",
+    "ran over my"
+  ];
+  let possession = ["homework", "toe", ""];
+  let where = ["on the street", "in my bathroom", "in my car", "on my roof"];
+
+  let pronounIndex = 0;
+  let subjectIndex = 0;
+  let actionIndex = 0;
+  let possessionIndex = 0;
+  let whereIndex = 0;
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possession[possessionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
